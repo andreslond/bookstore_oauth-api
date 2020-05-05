0c43bc7ec2305cc1e119aaef3e7f3eb1d85be83a
Andres Felipe Torres <andres.torres@ceiba.com.co>
2020-05-05 15:12:13 -0500

    Se configura el acceso a la base de datos Cassandra para el manejo de tokens de usuarios.

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -1,6 +1,10 @@
 package access_token
 
-import "time"
+import (
+	"github.com/andrestor2/bookstore_users-api/utils/errors"
+	"strings"
+	"time"
+)
 
 const (
 	expirationTime = 24
@@ -8,9 +12,26 @@ const (
 
 type AccessToken struct {
 	AccessToken string `json:"access_token"`
-	UserId      int64  `json:"user_d"`
+	UserId      int64  `json:"user_id"`
 	ClientId    int64  `json:"client_id"`
 	Expires     int64  `json:"expires"`
+}
+
+func (at *AccessToken) Validate() *errors.RestErr {
+	at.AccessToken = strings.TrimSpace(at.AccessToken)
+	if at.AccessToken == "" {
+		return errors.NewBadRequestError("invalid access token id")
+	}
+	if at.UserId <= 0 {
+		return errors.NewBadRequestError("invalid user id")
+	}
+	if at.ClientId <= 0 {
+		return errors.NewBadRequestError("invalid client id")
+	}
+	if at.Expires <= 0 {
+		return errors.NewBadRequestError("invalid expiration time")
+	}
+	return nil
 }
 
 func GetNewAccessToken() AccessToken {
--- src/http/access_token_http.go
+++ src/http/access_token_http.go
@@ -2,12 +2,14 @@ package http
 
 import (
 	"github.com/andrestor2/bookstore_oauth-api/src/domain/access_token"
+	"github.com/andrestor2/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type AccessTokenHandler interface {
-	GetById(c *gin.Context)
+	GetById(*gin.Context)
+	Create(*gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -21,10 +23,25 @@ func NewHandler(service access_token.Service) AccessTokenHandler {
 }
 
 func (handler *accessTokenHandler) GetById(c *gin.Context) {
-	accessToken, err := handler.service.GetById("access_token_id")
+	accessToken, err := handler.service.GetById(c.Param("access_token_id"))
 	if err != nil {
 		c.JSON(err.Status, err)
 		return
 	}
 	c.JSON(http.StatusOK, accessToken)
+}
+
+func (handler *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := handler.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusCreated, at)
 }
